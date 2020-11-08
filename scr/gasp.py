aeca2d066049d0b9f5840867c3bba1dbde38f9eb
vincent chen <vincentqchen@outlook.com>
2020-11-07 20:37:00 -0500

    hi

--- scr/gasp.py
+++ scr/gasp.py
@@ -115,15 +115,18 @@ class Bot(commands.Cog):
 	async def amicool(self, ctx):
 		if user_is_me(ctx):
 			await ctx.send(file = discord.File("res/wavy_bro.jpg"))
-			await ctx.send('ngl, your fucking wavy bro')
+			await ctx.send('ngl, your pretty wavy bro')
 		else:
 			if random.randint(1,10) == 2:
 				await ctx.send(file = discord.File('res/wavy_bro.jpg'))
-				await ctx.send('ngl, your fucking wavy bro')
+				await ctx.send('ngl, your pretty wavy bro')
 			else:
 				await ctx.send('https://media1.tenor.com/images/59926e8ff8929e782a58fc142769518c/tenor.gif?itemid=15200688')
 				await ctx.send('Nah bro, you ain\'t it')
 	
+	@commands.command()
+	async def poll(self, ctx, phrase:str=''):
+
 	@commands.command()
 	async def say(self, ctx, phrase:str=''):
 		return
