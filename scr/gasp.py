cc1671be509b5baca4c41338e65c885acc4738e6
vincent chen <vincentqchen@outlook.com>
2020-12-08 19:55:35 -0500

    fix's for my usage and master commands

--- scr/gasp.py
+++ scr/gasp.py
@@ -83,7 +83,7 @@ async def usageHelper(ctx,memberId,command):
 	userDoc = db.collection(u'users').document(str(memberId))
 	doc = userDoc.get()
 	if (doc.exists == False):
-		userDoc.set({u'meme':True, u'summon':True, u'stop':True, u'play':True, u'cry':True, u'invite':True, u'amicool':True, u'poll':True, u'scareme':True, u'say':True, u'noticemesenpai':True, u'clip':True, u'shaddup':True, u'swearat':True})
+		userDoc.set({u'meme':True, u'summon':True, u'stop':True, u'master':True, u'play':True, u'cry':True, u'invite':True, u'amicool':True, u'poll':True, u'scareme':True, u'say':True, u'noticemesenpai':True, u'clip':True, u'shaddup':True, u'swearat':True})
 		return True
 	else:
 		doc = doc.to_dict()
@@ -158,6 +158,7 @@ class Bot(commands.Cog):
 		# Create document if user doesn't exist
 		if (doc.exists == False):
 			userDoc.set({u'meme':True, u'master':False, u'summon':True, u'stop':True, u'play':True, u'cry':True, u'invite':True, u'amicool':True, u'poll':True, u'scareme':True, u'say':True, u'noticemesenpai':True, u'clip':True, u'shaddup':True, u'swearat':True})
+			docDict = userDoc.get().to_dict()
 		masterExists = False
 		for x in mDoc:
 			if x == str(ctx.author.id):
@@ -181,6 +182,17 @@ class Bot(commands.Cog):
 					embed.add_field(name="Disable usage", value="gasp usage \{user\} \{command\} disable", inline=False)
 					embed.set_footer(text="Requested by {0}".format(ctx.author.display_name))
 					await ctx.send(embed=embed)
+			elif args[0] == 'all':
+				if args[1] == 'enable':
+					userDoc.set({u'meme':True, u'master':False, u'summon':True, u'stop':True, u'play':True, u'cry':True, u'invite':True, u'amicool':True, u'poll':True, u'scareme':True, u'say':True, u'noticemesenpai':True, u'clip':True, u'shaddup':True, u'swearat':True})
+				elif args[1] == 'disable':
+					userDoc.set({u'meme':False, u'master':False, u'summon':False, u'stop':False, u'play':False, u'cry':False, u'invite':False, u'amicool':False, u'poll':False, u'scareme':False, u'say':False, u'noticemesenpai':False, u'clip':False, u'shaddup':False, u'swearat':False})
+				else:
+					embed=discord.Embed(title="Usage Command", description="Prevent specific users from using commands", color=0x1553e5)
+					embed.add_field(name="Enable usage", value="gasp usage \{user\} \{ommand\} enable", inline=False)
+					embed.add_field(name="Disable usage", value="gasp usage \{user\} \{command\} disable", inline=False)
+					embed.set_footer(text="Requested by {0}".format(ctx.author.display_name))
+					await ctx.send(embed=embed)
 			else:
 				embed=discord.Embed(title="Usage Command", description="The {} command does not exist".format(args[0]), color=0x59ff00)
 				embed.set_footer(text="Requested by {0}".format(ctx.author.display_name))
@@ -197,7 +209,7 @@ class Bot(commands.Cog):
 
 		response = requests.get("https://api.imgflip.com/get_memes")
 		memetemplates = ["","","","","",""]
-		memedic = {}  #{num: (id,boxcount)}
+		memedic = {}  #{num: (id,boxcount)}b
 
 
 		# post = {
